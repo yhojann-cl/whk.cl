46e60e0fa8c40bdf941d44ecfb8e0cce604d80f8
Yhojann Aguilera <yhojann.aguilera@gmail.com>
2021-10-29 16:22:18 -0300

    Primer commit

---
+++ public_html/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+    <!--
+    ██╗    ██╗██╗  ██╗██╗  ██╗
+    ██║    ██║██║  ██║██║ ██╔╝
+    ██║ █╗ ██║███████║█████╔╝ 
+    ██║███╗██║██╔══██║██╔═██╗ 
+    ╚███╔███╔╝██║  ██║██║  ██╗
+     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+      - The WhiteHacKer -
+    -->
+    <head>
+        <title>WHK</title>
+        <meta charset="utf-8" />
+        <meta
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            name="viewport" />
+        <style type="text/css" media="all">
+            body{
+                text-align: center;
+                background-color: #0a0b11;
+                color: #eee;
+                font-family: Monospace;
+                font-size: 12px;
+                padding: 0;
+                margin: 0;
+            }
+            img{ padding: 7% 10px 0 10px; max-width: 90%; }
+            p{ font-size: 16px; margin: 0; padding: 30px 0 0 0; }
+        </style>
+    </head>
+    <body>
+        <img src="/bg.jpg" />
+        <p>Follow the white rabit.</p>
+    </body>
+</html>
